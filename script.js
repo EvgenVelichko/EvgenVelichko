8d1ddee523f575d6663a2b8f298a64402b293adb
Evgen <evgenvelicko4@gmail.com>
2024-08-22 22:29:00 +0300

    Burger menu

--- script.js
+++ script.js
@@ -51,3 +51,38 @@ document.querySelectorAll(".nav-links a").forEach((link) => {
     body.classList.remove("lock-scroll");
   });
 });
+let currentProject = 0;
+
+function showProject(index) {
+    const projects = document.querySelectorAll('.project');
+    const totalProjects = projects.length;
+
+    if (index >= totalProjects) {
+        currentProject = 0; 
+    } else if (index < 0) {
+        currentProject = totalProjects - 1; 
+    } else {
+        currentProject = index;
+    }
+
+    const slider = document.querySelector('.project-slider');
+    const projectWidth = projects[0].offsetWidth; 
+
+    slider.style.transform = `translateX(-${currentProject * projectWidth}px)`;
+
+    
+    projects.forEach((project, i) => {
+        project.classList.toggle('active', i === currentProject);
+    });
+}
+
+function nextProject() {
+    showProject(currentProject + 1);
+}
+
+function prevProject() {
+    showProject(currentProject - 1); 
+}
+
+
+showProject(currentProject);
