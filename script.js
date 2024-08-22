6411205ef776861ddeadefe03f4aaa77fbc5baee
Evgen <evgenvelicko4@gmail.com>
2024-08-22 13:24:35 +0300

    Test Burger menu

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const requestInterval = 5000;
 function makeRequest() {
   const currentTime = new Date().getTime();
   if (currentTime - lastRequestTime > requestInterval) {
-    
     lastRequestTime = currentTime;
   } else {
     alert("Слишком много запросов. Пожалуйста, подождите.");
@@ -19,10 +18,11 @@ function checkIp(ip) {
   }
   ipRequests[ip].push(currentTime);
 
-  
-  ipRequests[ip] = ipRequests[ip].filter(time => currentTime - time < requestInterval);
+  ipRequests[ip] = ipRequests[ip].filter(
+    (time) => currentTime - time < requestInterval
+  );
 
-  if (ipRequests[ip].length > 5) { 
+  if (ipRequests[ip].length > 5) {
     alert("Ваш IP заблокирован из-за подозрительной активности.");
     return false;
   }
@@ -31,7 +31,23 @@ function checkIp(ip) {
 
 function makeRequest(ip) {
   if (checkIp(ip)) {
-    
     console.log("Запрос выполнен");
   }
 }
+const burgerMenu = document.querySelector(".burger-menu");
+const navLinks = document.querySelector(".nav-links");
+const body = document.body;
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
+  navLinks.classList.toggle("active");
+  body.classList.toggle("lock-scroll");
+});
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    burgerMenu.classList.remove("active");
+    navLinks.classList.remove("active");
+    body.classList.remove("lock-scroll");
+  });
+});
