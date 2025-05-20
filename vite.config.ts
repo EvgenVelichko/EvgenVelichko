4f5dc7fa55363497fe90df42ba042cbe740df8ea
Evgen Velichko <evgenvelicko4@gmail.com>
2025-05-20 21:36:20 +0300

    test gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/evgenvelychko/',
+    base: '/evgenvelichko/',
     optimizeDeps: {
         exclude: ['lucide-react'],
     },
